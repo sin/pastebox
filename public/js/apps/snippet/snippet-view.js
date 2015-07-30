43218d2c0858311a7186057c2e16c3c8b02356cd
sin <mateusz@sienkan.pl>
2015-07-30 16:50:20 +0200

    Added events to snippet view, can delete snippet now

--- public/js/apps/snippet/snippet-view.js
+++ public/js/apps/snippet/snippet-view.js
@@ -3,6 +3,34 @@ define(['jquery', 'underscore', 'backbone', 'marionette', 'prism', 'global', 'te
         return Marionette.ItemView.extend({
             tagName: 'span',
             template: template,
+
+            events: {
+                'click .link': 'link',
+                'click .fork': 'fork',
+                'click .edit': 'edit',
+                'click .delete': 'delete'
+            },
+
+            link: function (event) {
+                event.preventDefault();
+            },
+
+            fork: function (event) {
+                event.preventDefault();
+            },
+
+            edit: function (event) {
+                event.preventDefault();
+            },
+
+            delete: function (event) {
+                event.preventDefault();
+                this.model.destroy()
+                    .success(function () {
+                        app.trigger('snippets:new');
+                    });
+            },
+
             onRender: function () {
                 prism.highlightElement($(this.el).find('code')[0]);
             }
