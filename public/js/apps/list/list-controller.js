cf55c11fc064fec43ccd20ae63b40215f53f5451
sin <mateusz@sienkan.pl>
2015-07-30 10:08:31 +0200

    Initialize snippets list view with collection

--- public/js/apps/list/list-controller.js
+++ public/js/apps/list/list-controller.js
@@ -1,10 +1,14 @@
 define(['jquery', 'underscore', 'backbone', 'marionette', 'global', 'apps/list/list-view',
-        'apps/main/main-router'],
-    function ($, _, Backbone, Marionette, app, View, mainRouter) {
+        'apps/main/main-router', 'collections/snippets'],
+    function ($, _, Backbone, Marionette, app, View, mainRouter, Snippets) {
         var show = function () {
-            var view = new View()
-            mainRouter.show();
-            console.log(app.main)
+            var snippets = new Snippets(),
+                view = new View({collection: snippets});
+
+            if (!app.main.hasView()) {
+                mainRouter.show();
+            }
+
             app.main.currentView.list.show(view);
         };
 
