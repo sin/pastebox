deffe927dbf7bfb756f5567f8539d0bf4645a38a
sin <mateusz@sienkan.pl>
2015-07-31 16:43:22 +0200

    Added sorting snippets collection

--- public/js/apps/snippets/snippets-view.js
+++ public/js/apps/snippets/snippets-view.js
@@ -6,7 +6,43 @@ define(['jquery', 'underscore', 'backbone', 'marionette',
         return Marionette.CompositeView.extend({
             template: template,
             childView: ChildView,
-            childViewContainer: '.snippets-list'
+            childViewContainer: '.snippets-list',
+
+            events: {
+                'click .sort': 'fetchSortedBy'
+            },
+
+            initialize: function () {
+                this.state = {
+                    sortBy: 'created',
+                    sortOrder: 0
+                };
+            },
+
+            serializeData: function () {
+                var s = this.state;
+                return {
+                    created: s.sortBy === 'created',
+                    updated: s.sortBy === 'updated',
+                    title: s.sortBy === 'title',
+                    author: s.sortBy === 'author',
+                    desc: s.sortOrder
+                };
+            },
+
+            fetchSortedBy: function (event) {
+
+                var sortBy = $(event.target).data('sort'),
+                    sortOrder = this.state.sortOrder ? 0 : 1;
+
+                this.state.sortBy = sortBy;
+                this.state.sortOrder = sortOrder;
+
+                this.collection.fetch({data: $.param(this.state)});
+                this.render();
+
+                event.preventDefault();
+            }
         });
 
     });
